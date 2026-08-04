5c8d1d2eb37ae8f771724dc0924719de88efd9f9
Bruno Riul <brunoc.riul@gmail.com>
2026-08-04 08:04:29 -0300

    Primeiro Commit - Salvando

---
+++ src/main/java/br/gm/brunoriul/estoqueSAEP/EstoqueSaepApplication.java
@@ -0,0 +1,13 @@
+package br.gm.brunoriul.estoqueSAEP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueSaepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueSaepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gm/brunoriul/estoqueSAEP/EstoqueSaepApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gm.brunoriul.estoqueSAEP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueSaepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
